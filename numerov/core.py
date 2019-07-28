b65c48c3eb9a4a195418921c3421fbff50dd1189
Adam <adam@pop-os.localdomain>
2019-07-28 21:19:47 +0100

    new function : radial_integral

--- numerov/core.py
+++ numerov/core.py
@@ -7,27 +7,23 @@ Created on Fri Aug  4 16:20:34 2017
 from math import exp, ceil, log
 import numpy as np
 
-def wf_numerov(n, l, nmax, step=0.005, rmin=0.65):
-    """ Use the Numerov method to find the wavefunction for state n*, l, where
-        n* = n - delta.
-        nmax ensures that wavefunctions from different values of n can be aligned.
+def radial_wf(n, l, step=0.005, rmin=0.65):
+    """ Use the Numerov method to find the wavefunction of
+        
+        | n, l ⟩
+
+    For states with a quantum defect, swap n for n_eff = n - qd
     """
     w1 = -0.5 * n**-2.0
     w2 = (l + 0.5)**2.0
-    rmax = 2 * nmax * (nmax + 15)
+    rmax = 2 * n * (n + 15)
     r_in = n**2.0 - n * (n**2.0 - l*(l + 1.0))**0.5
     step_sq = step**2.0
-    # ensure wf arrays will align using nmax
-    if n == nmax:
-        i = 0
-        r_sub2 = rmax
-    else:
-        i = int(ceil(log(rmax / (2 * n * (n + 15))) / step))
-        r_sub2 = rmax * exp(-i*step)
-    i += 1
 
     # initialise
-    r_sub1 = rmax * exp(-i*step)
+    i = 1
+    r_sub2 = rmax
+    r = r_sub1 = rmax * exp(-i*step)
     rvals = [r_sub2, r_sub1]
     g_sub2 = 2.0 * r_sub2**2.0 * (-1.0 / r_sub2 - w1) + w2
     g_sub1 = 2.0 * r_sub1**2.0 * (-1.0 / r_sub1 - w1) + w2
@@ -37,7 +33,6 @@ def wf_numerov(n, l, nmax, step=0.005, rmin=0.65):
 
     # Numerov method
     i += 1
-    r = r_sub1
     drr = exp(-step)**(-l - 1) - 1.0
     while r >= rmin:
         ## next step
@@ -68,3 +63,88 @@ def wf_numerov(n, l, nmax, step=0.005, rmin=0.65):
     # normalisation
     yvals = yvals * (rvals * np.sum(yvals**2.0 * rvals**2.0))**-0.5 / step**0.5
     return rvals, yvals
+
+def radial_integral(n1, l1, n2, l2, step=0.005, rmin=0.65, p=1):
+    """ Use the Numerov method to calculate 
+        
+        ⟨ n2, l2 | r^p | n1, l1 ⟩
+    """
+    w11 = -0.5 * n1**-2.0
+    w12 = (l1 + 0.5)**2.0
+    w21 = -0.5 * n2**-2.0
+    w22 = (l2 + 0.5)**2.0
+    nmax = max(n1, n2)
+    rmax = 2 * nmax * (nmax + 15)
+    r_in1 = n1**2.0 - n1 * (n1**2.0 - l1*(l1 + 1.0))**0.5
+    r_in2 = n2**2.0 - n2 * (n2**2.0 - l2*(l2 + 1.0))**0.5
+    step_sq = step**2.0
+
+    # initialise
+    i = 1
+    r_sub2 = rmax
+    r_sub1 = rmax * exp(-i*step)
+    
+    g1_sub2 = 2.0 * r_sub2**2.0 * (-1.0 / r_sub2 - w11) + w12
+    g1_sub1 = 2.0 * r_sub1**2.0 * (-1.0 / r_sub1 - w11) + w12
+    g2_sub2 = 2.0 * r_sub2**2.0 * (-1.0 / r_sub2 - w21) + w22
+    g2_sub1 = 2.0 * r_sub1**2.0 * (-1.0 / r_sub1 - w21) + w22
+    
+    y1_sub2 = 1e-10
+    y1_sub1 = y1_sub2 * (1.0 + step * g1_sub2**0.5)
+    y2_sub2 = 1e-10
+    y2_sub1 = y2_sub2 * (1.0 + step * g2_sub2**0.5)    
+    
+    norm1 = (y1_sub2**2.0 * r_sub2**2.0 
+             + y1_sub1**2.0 * r_sub1**2.0)
+    norm2 = (y2_sub2**2.0 * r_sub2**2.0 
+             + y2_sub1**2.0 * r_sub1**2.0)
+   
+    integral = (y1_sub2 * y2_sub2 * r_sub2**(2.0 + p)
+                + y1_sub1 * y2_sub1 * r_sub1**(2.0 + p))
+
+    i += 1
+    r = r_sub1
+    while r >= rmin:
+        # Numerov method
+        r = rmax * exp(-i*step)
+        g1 = 2.0 * r**2.0 * (-1.0 / r - w11) + w12
+        g2 = 2.0 * r**2.0 * (-1.0 / r - w21) + w22
+        y1 = ((y1_sub2 * (g1_sub2 - (12.0 / step_sq))
+             + y1_sub1 * (10.0 * g1_sub1 + (24.0 / step_sq)))
+             / ((12.0 / step_sq) - g1))
+        y2 = ((y2_sub2 * (g2_sub2 - (12.0 / step_sq))
+             + y2_sub1 * (10.0 * g2_sub1 + (24.0 / step_sq)))
+             / ((12.0 / step_sq) - g2))
+ 
+        # check for divergence
+        if r < r_in1:
+            dy1 = abs((y1 - y1_sub1) / y1_sub1)
+            dr = (r**(-l1 - 1) - r_sub1**(-l1 - 1)) / r_sub1**(-l1 - 1)
+            if dy1 > dr:
+                break
+
+        if r < r_in2:
+            dy2 = abs((y2 - y2_sub1) / y2_sub1)
+            dr = (r**(-l2 - 1) - r_sub1**(-l2 - 1)) / r_sub1**(-l2 - 1)
+            if dy2 > dr:
+                break
+                
+        # store vals
+        norm1 += y1**2.0 * r**2.0
+        norm2 += y2**2.0 * r**2.0
+        integral += y1 * y2 * r**(2.0 + p)
+        
+        # next iteration
+        r_sub1 = r
+
+        g1_sub2 = g1_sub1
+        g1_sub1 = g1
+        g2_sub2 = g2_sub1
+        g2_sub1 = g2
+        
+        y1_sub2 = y1_sub1
+        y1_sub1 = y1
+        y2_sub2 = y2_sub1
+        y2_sub1 = y2
+        i += 1
+    return integral * (norm1 * norm2)**-0.5
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='numerov',
-      version='0.0.1',
+      version='0.0.2',
       description='numerov wf solver',
       author='Adam Deller',
       author_email='a.deller@ucl.ac.uk',
